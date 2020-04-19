2ce7b04c1abd848d1b23a83afa81b1507ab57461
Jenny Tang <jennydtang@gmail.com>
2020-04-19 15:16:43 -0400

    updated chain

--- promise_chain .js
+++ promise_chain .js
@@ -14,7 +14,7 @@ console.log(testNum(49));
 // The first function, makeAllCaps, will take in an array of words and capitalize them. 
 //A second function, sortWords, will sort the words in alphabetical order. 
 // If the array contains anything besides a single word (string), an error should be thrown.
-function makeAllCaps(words){
+const makeAllCaps= function(words){
     return new Promise(function(resolve, reject){
         if(words.every(function(str1) {//every method checks if every item of the array passes test
             return typeof str1 ==="string";//make function to test string; typeof operate tests data type
@@ -30,7 +30,7 @@ function makeAllCaps(words){
         }
     });
 };
-function sortWords(words) {
+const sortWords = function(words) {
     return new Promise(function(resolve, reject) {
       if (words) {
         resolve(words.sort()); //if it passes, sort the words
@@ -40,11 +40,22 @@ function sortWords(words) {
     });
   };
 
-var words=["you", "always", "be", "beautiful"];
+var words=["you", "always","be", "beautiful"];
+var wantWords=["want", 3, "words"];
+
 
 makeAllCaps(words)
 .then(sortWords(words))//chaining
     .then(function(result){ //then, function to print result
-    console.log(result);
+    console.log(result)})
+    .catch(function(error){ //this catches the error
+        console.log(error);
 });
-  
+
+makeAllCaps(wantWords)
+.then(sortWords(wantWords))//chaining
+    .then(function(result){ //then, function to print result
+    console.log(result)})
+    .catch(function(error){ //this catches the error
+        console.log(error);
+});
